dc3178227dd5440293678e0142a6ca2741b51092
Gianluca Ciardi <Gianlucaciardi93@gmail.com>
2022-04-04 12:45:01 +0200

    LunediMattina

---
+++ src/main/java/com/corso/java/hibernate/Event.java
@@ -0,0 +1,57 @@
+package com.corso.java.hibernate;
+
+import java.util.Date;
+
+public class Event {
+
+    private Long id;
+    private Date date;
+    private String title;
+    private String description;
+
+    public Event(){}
+
+    public Event(Long id, Date date, String title, String description) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Event(String un_evento_seguente, String s, Date date) {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
+
